655e23a1618a3209aae0eb3d6cef27b256b0532f
Justin Brooks <justin@datatorch.io>
2020-05-05 22:44:25 -0400

    created release script

---
+++ scripts/release.sh
@@ -0,0 +1,9 @@
+rm -rf ../files-to-gh
+mkdir ../files-to-gh
+helm package datatorch/
+cp datatorch-*.tgz ../files-to-gh
+rm datatorch-*.tgz
+helm repo index ../files-to-gh --url https://charts.datatorch.io
+git stash
+git checkout gh-pages
+yes | cp -rf ../files-to-gh ./
