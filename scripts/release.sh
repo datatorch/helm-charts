a9d98735b1ab1c34c0c8e52ead0635f7f8c40c04
Justin Brooks <jsbroks@gmail.com>
2020-05-05 23:23:42 -0400

    added merge

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,12 @@
 rm -rf ../files-to-gh
 mkdir ../files-to-gh
-helm package datatorch/
-cp datatorch-*.tgz ../files-to-gh
-rm datatorch-*.tgz
-helm repo index ../files-to-gh --url https://charts.datatorch.io
-git stash
+helm package -d ../files-to-gh datatorch/
 git checkout gh-pages
-yes | cp -rf ../files-to-gh/* ./
+helm repo index --url "https://charts.datatorch.io" --merge index.yaml ../files-to-gh
+
+mv ../files-to-gh/*.tgz ./
+mv ../files-to-gh/index.yaml ./index.yaml
+
+# git stash
+# git checkout gh-pages
+# yes | cp -rf ../files-to-gh/* ./
