7e056a72e8079ce4a68315c98a43d6fb2bc949b9
Justin Brooks <justin@datatorch.io>
2020-06-04 13:46:59 -0400

    updated config

--- scripts/publish-configs.sh
+++ scripts/publish-configs.sh
@@ -2,6 +2,6 @@ az artifacts universal publish \
     --organization https://dev.azure.com/DataTorchio/ \
     --feed datatorchio \
     --name helm-chart-values \
-    --version 0.1.0 \
+    --version 0.1.1 \
     --description "Helm chart values" \
     --path ./configs
