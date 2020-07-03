d80d0b883b7ac3ce6f1e403187960c22fa62e3da
Justin Brooks <justin@datatorch.io>
2020-07-02 21:49:31 -0400

    added resource options

--- scripts/update-tag.py
+++ scripts/update-tag.py
@@ -1,11 +1,14 @@
+"""
+Simple script to update datatorch container tags.
+"""
 import sys
 from ruamel.yaml import YAML
 
 # RECENT_BUILD = 'https://dev.azure.com/datatorchio/datatorch/_apis/build/builds?api-version=5.0&$top=20&statusFilter=completed'
-PATH = './datatorch/values.yaml'
+PATH = "./datatorch/values.yaml"
 
 if len(sys.argv) != 3:
-    sys.exit('container and tag is required.')
+    sys.exit("container and tag is required.")
 
 container = sys.argv[1]
 tag = sys.argv[2]
@@ -16,9 +19,9 @@ yaml.preserve_quotes = True
 with open(PATH) as fp:
     data = yaml.load(fp)
 
-data[container]['tag'] = tag
+data[container]["tag"] = tag
 
-with open(PATH, 'w') as fp:
+with open(PATH, "w") as fp:
     yaml.dump(data, fp)
 
-print(f'Done writing to {PATH} {container}.tag = {tag}')
+print(f"Done writing to {PATH} {container}.tag = {tag}")
