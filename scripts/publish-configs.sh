e9c003baf6b1bec8c9cf44ff16b81bb85332ecb3
Github <jsbroks@gmail.com>
2020-10-05 10:54:33 -0400

    chore: updated config

--- scripts/publish-configs.sh
+++ scripts/publish-configs.sh
@@ -2,6 +2,6 @@ az artifacts universal publish \
     --organization https://dev.azure.com/DataTorchio/ \
     --feed datatorchio \
     --name helm-chart-values \
-    --version 0.1.4 \
+    --version 0.1.5 \
     --description "Helm chart values" \
     --path ./configs
