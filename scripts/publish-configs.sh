3dfc57c66cd1007d3c6445f3ac94b31498952817
Justin Brooks <jsbroks@gmail.com>
2020-10-15 20:58:41 -0400

    chore: updated configs

--- scripts/publish-configs.sh
+++ scripts/publish-configs.sh
@@ -2,6 +2,6 @@ az artifacts universal publish \
     --organization https://dev.azure.com/DataTorchio/ \
     --feed datatorchio \
     --name helm-chart-values \
-    --version 0.1.5 \
-    --description "Helm chart values" \
+    --version 0.1.6 \
+    --description "Helm chart config values" \
     --path ./configs
