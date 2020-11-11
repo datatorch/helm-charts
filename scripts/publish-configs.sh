2233750d250f9b96410c66d4d89556d03abed640
Justin Brooks <justin@datatorch.io>
2020-11-10 19:19:05 -0500

    chore: remove ingress rewrite

--- scripts/publish-configs.sh
+++ scripts/publish-configs.sh
@@ -2,6 +2,6 @@ az artifacts universal publish \
     --organization https://dev.azure.com/DataTorchio/ \
     --feed datatorchio \
     --name helm-chart-values \
-    --version 0.1.8 \
+    --version 0.1.11 \
     --description "Helm chart config values" \
     --path ./configs
