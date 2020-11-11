858a6963985d8c526dd7e029add887be40ce629f
Justin Brooks <justin@datatorch.io>
2020-11-10 20:05:20 -0500

    chore: remove fqdn

--- scripts/publish-configs.sh
+++ scripts/publish-configs.sh
@@ -2,6 +2,6 @@ az artifacts universal publish \
     --organization https://dev.azure.com/DataTorchio/ \
     --feed datatorchio \
     --name helm-chart-values \
-    --version 0.1.11 \
+    --version 0.1.12 \
     --description "Helm chart config values" \
     --path ./configs
